df3680bdf13bcc1dcff304742cf48e5eeb34d20c
mlavin <markdlavin@gmail.com>
2012-05-17 20:15:43 -0400

    Simplify and rename core model.

--- scribbler/models.py
+++ scribbler/models.py
@@ -1,18 +1,13 @@
 "Models for storing snippet content."
 
-import datetime
-
-from django.contrib.auth.models import User
 from django.db import models
 
 
-class Snippet(model.Model):
+class Scribble(models.Model):
     "Core model for storing snippet content."
 
     created_time = models.DateTimeField(auto_now_add=True, editable=False)
-    created_by = models.ForeignKey(User, editable=False)
     modified_time = models.DateTimeField(auto_now=True, editable=False)
-    modified_by = models.ForeignKey(User, editable=False)
     name = models.CharField(max_length=255, blank=True, default=u"")
     slug = models.SlugField(max_length=255, blank=True, default=u"")
     url = models.CharField(max_length=255, blank=True, default=u"")
