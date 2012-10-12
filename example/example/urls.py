480dda887e7287a92ec4f82c6c9219c337fafed4
juliaelman <julia.elman@gmail.com>
2012-10-12 13:46:07 -0400

    Adding  more styles for example site.

--- example/example/urls.py
+++ example/example/urls.py
@@ -4,8 +4,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from django.views.generic.base import TemplateView
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'example.views.home', name='home'),
@@ -17,5 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^scribble/', include('scribbler.urls')),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', 'example.views.homepage', name='home'),
 )
