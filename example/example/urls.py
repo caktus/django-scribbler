68cff705e36f0222b92224bb24cb3be9480a13a4
Mark Lavin <mlavin@caktusgroup.com>
2012-11-30 10:05:24 -0500

    Update example project to include i18n urls.

--- example/example/urls.py
+++ example/example/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+js_info_dict = {
+    'packages': ('scribbler', ),
+}
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'example.views.home', name='home'),
@@ -15,5 +19,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^scribble/', include('scribbler.urls')),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
     url(r'^$', 'example.views.homepage', name='home'),
 )
