f85d784694eab5ddb296d29129851efc00954c1c
mlavin <markdlavin@gmail.com>
2013-02-02 12:02:07 -0500

    Starting QUnit test suite for scribbler menu. See #32

---
+++ scribbler/tests/qunit/main.js
@@ -0,0 +1,57 @@
+/*global require, QUnit*/
+
+require.config({
+    baseUrl: "../../static/scribbler/js/",
+    paths: {
+        jquery: '../libs/jquery',
+        codemirror: '../libs/codemirror/lib/codemirror',
+        jsmode: '../libs/codemirror/mode/javascript/javascript',
+        cssmode: '../libs/codemirror/mode/css/css',
+        xmlmode: '../libs/codemirror/mode/xml/xml',
+        htmlmode: '../libs/codemirror/mode/htmlmixed/htmlmixed',
+        simplehint: '../libs/codemirror/lib/util/simple-hint',
+        scribblereditor: 'scribbler-editor',
+        scribblermenu: 'scribbler-menu',
+        djangohint: 'djangohint',
+        backbone: '../libs/backbone',
+        underscore: '../libs/underscore'
+    },
+    shim: {
+        codemirror: {
+            exports: 'CodeMirror'
+        },
+        simplehint: {
+            exports: 'CodeMirror',
+            deps: ['codemirror']
+        },
+        jsmode: {
+            exports: 'CodeMirror',
+            deps: ['codemirror']
+        },
+        cssmode: {
+            exports: 'CodeMirror',
+            deps: ['codemirror']
+        },
+        xmlmode: {
+            exports: 'CodeMirror',
+            deps: ['codemirror']
+        },
+        htmlmode: {
+            exports: 'CodeMirror',
+            deps: ['xmlmode', 'jsmode', 'cssmode']
+        },
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        underscore: {
+            exports: '_'
+        }
+    }
+});
+
+require(['menu-test.js'], function () {
+    //Tests loaded, run Tests
+    QUnit.load();
+    QUnit.start();
+});
