2cabf243b35056babd7574914a2c707518dd5236
mlavin <markdlavin@gmail.com>
2013-02-02 20:16:36 -0500

    Testing editor preview.

--- scribbler/tests/qunit/editor-test.js
+++ scribbler/tests/qunit/editor-test.js
@@ -33,8 +33,73 @@ define(['jquery', 'underscore', 'scribblereditor'], function ($, _, ScribbleEdit
     test("Editor Open", function () {
         var content = $('#id_header-content', this.scribble).val();
         expect(2);
-        editor.open(scribble);
+        this.editor.open(this.scribble);
         ok(this.editor.visible, "Editor should be visible.");
         equal(this.editor.editor.getValue(), content, "Editor should have scribble content.");
+    });
+
+    test("Editor Close", function () {
+        expect(2);
+        this.editor.open(this.scribble);
+        this.editor.close();
+        ok(!this.editor.visible, "Editor should not be visible.");
+        equal(this.editor.editor.getValue(), '', "Editor should be empty.");
+    });
+
+    test("Editor Submit Preview", function () {
+        expect(2);
+        this.editor.open(this.scribble);
+        this.editor.rendering = false;
+        this.requests.length = 0;
+        this.editor.submitPreview();
+        equal(this.requests.length, 1, "Request submitted for preview.");
+        equal(this.requests[0].url, '/scribble/preview/');
+    });
+
+    test("Editor Currently Rendering", function () {
+        expect(1);
+        this.editor.open(this.scribble);
+        this.editor.rendering = true;
+        this.requests.length = 0;
+        this.editor.submitPreview();
+        equal(this.requests.length, 0, "No preview should be submitted.");
+    });
+
+    test("Editor Force Preview", function () {
+        expect(2);
+        this.editor.open(this.scribble);
+        this.editor.rendering = true;
+        this.requests.length = 0;
+        this.editor.submitPreview(true);
+        equal(this.requests.length, 1, "Force the preview submission.");
+        equal(this.requests[0].url, '/scribble/preview/');
+    });
+
+    test("Editor Preview Success", function () {
+        var preview = $('.preview', this.scribble);
+        expect(1);
+        this.editor.open(this.scribble);
+        this.editor.rendering = false;
+        this.requests.length = 0;
+        this.editor.submitPreview();
+        this.requests[0].respond(200, {"Content-Type": "application/json"},
+             '{"valid": true, "html": "Foo", "variables": []}'
+        );
+        equal(preview.html(), 'Foo', "Preview div should be updated.");
+    });
+
+    test("Editor Preview Error", function () {
+        var preview = $('.preview', this.scribble);
+        var original = preview.html();
+        expect(2);
+        this.editor.open(this.scribble);
+        this.editor.rendering = false;
+        this.requests.length = 0;
+        this.editor.submitPreview();
+        this.requests[0].respond(200, {"Content-Type": "application/json"},
+            '{"valid": false, "html": "", "error": {"message": "Bar", "line": 1}}'
+        );
+        equal(preview.html(), original, "Preview div should be unchanged.");
+        equal(this.editor.controls.errors.text(), "Error: Bar", "Error message should be set.");
     });
 });
