e4aa92da371c7e0ab7c65c7702504872aa31b9b2
mlavin <markdlavin@gmail.com>
2013-02-03 22:16:04 -0500

    Get list of plugins from script data- and dynamically load.

--- scribbler/static/scribbler/js/scribbler.js
+++ scribbler/static/scribbler/js/scribbler.js
@@ -62,13 +62,34 @@ require.config({
 require(['jquery', 'scribblereditor', 'scribblermenu'], function ($, ScribbleEditor, ScribbleMenu) {
     'use strict';
 
+    var pluginlist = [],
+        script;
+
     $.noConflict(true);
+
+    // Dynamically loads additional plugins for django-scribbler
+    function pluginLoader(name, editor, menu) {
+        var path = "./plugins/" + name;
+        require([path], function (plugin) {
+            plugin.call(null, editor, menu);
+        });
+    }
+
+    script = $("script[data-scribbler-plugins]");
+
+    if (script.length) {
+        pluginlist = script.data("scribblerPlugins").split(",");
+    }
 
     $(document).ready(function () {
         var editor = new ScribbleEditor(),
             menu = new ScribbleMenu();
-        editor.render();
         editor.bind("open", menu.close, menu);
+        function executePlugin(name) {
+            pluginLoader(name, editor, menu);
+        }
+        pluginlist.map(executePlugin);
+        editor.render();
         menu.render();
     });
 });
