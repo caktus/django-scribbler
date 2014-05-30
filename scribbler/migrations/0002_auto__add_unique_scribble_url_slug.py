13b616237740c3535fc9cd66cd27c80c127b1478
Mark Lavin <mlavin@caktusgroup.com>
2014-05-30 16:47:46 -0400

    Removing u'' prefixes in the migration files.

--- scribbler/migrations/0002_auto__add_unique_scribble_url_slug.py
+++ scribbler/migrations/0002_auto__add_unique_scribble_url_slug.py
@@ -24,13 +24,13 @@ class Migration(SchemaMigration):
     models = {
         'scribbler.scribble': {
             'Meta': {'unique_together': "((u'slug', u'url'),)", 'object_name': 'Scribble'},
-            'content': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'}),
+            'content': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
             'created_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'modified_time': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'}),
-            'url': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'})
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
         }
     }
 
