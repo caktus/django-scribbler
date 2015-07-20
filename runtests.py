9946d27b75e589482d0bd05429f8247dc7f6f05e
Yaroslav Klyuyev <imposeren@gmail.com>
2015-07-20 23:53:50 +0300

    fixed tests for django 1.8

--- runtests.py
+++ runtests.py
@@ -45,12 +45,7 @@ if not settings.configured:
                 'OPTIONS': {
                     'context_processors': [
                         'django.contrib.auth.context_processors.auth',
-                        'django.template.context_processors.debug',
-                        'django.template.context_processors.i18n',
-                        'django.template.context_processors.media',
-                        'django.template.context_processors.static',
-                        'django.template.context_processors.tz',
-                        'django.contrib.messages.context_processors.messages',
+                        'django.core.context_processors.request',
                     ],
                     'debug': False,
                 },
