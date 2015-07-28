55b1d5f6a4264351ab2070739682faf8a70b2485
Tobias McNulty <tobias@caktusgroup.com>
2015-07-28 10:57:37 -0400

    remove patterns() from urls.py

--- scribbler/urls.py
+++ scribbler/urls.py
@@ -2,15 +2,15 @@ from __future__ import unicode_literals
 
 try:
     # Django 1.4+
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError: # pragma: no cover
     # Django 1.3
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 
 from scribbler import views
 
 
-urlpatterns = patterns('',
+urlpatterns = (
     url('^preview/$', views.preview_scribble, name='preview-scribble'),
     url('^create/$', views.create_edit_scribble, name='create-scribble'),
     url('^edit/(?P<scribble_id>(\d+))/$', views.create_edit_scribble, name='edit-scribble'),
