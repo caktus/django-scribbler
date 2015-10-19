f5fbebc86a8cab2f65dc9c6f83c5e74d1c52fead
bphillips <bphillips@caktusgroup.com>
2015-10-19 12:02:07 -0400

    106 - Added context processors to runtests.py TEMPLATES setting to enable js elements(eg. backbone)

--- runtests.py
+++ runtests.py
@@ -52,6 +52,11 @@ if not settings.configured:
                 'OPTIONS': {
                     'context_processors': [
                         'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                        'django.core.context_processors.debug',
+                        'django.core.context_processors.media',
+                        'django.core.context_processors.i18n',
+                        'django.core.context_processors.static',
                         'django.core.context_processors.request',
                     ],
                     'debug': False,
