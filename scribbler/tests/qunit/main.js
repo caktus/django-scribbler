c123d3fc87285424a61eb8f1cf291b8d39fc632a
bphillips <bphillips@caktusgroup.com>
2015-11-04 09:18:05 -0500

    Removed unnecessary QUnit.load()

--- scribbler/tests/qunit/main.js
+++ scribbler/tests/qunit/main.js
@@ -2,7 +2,6 @@
 var menuTest = require('./menu-test.js');
 var editorTest = require('./editor-test.js');
     //Tests loaded, run Tests
-    QUnit.load();
-    QUnit.start();
-    menuTest();
-    editorTest();
+QUnit.start();
+menuTest();
+editorTest();
